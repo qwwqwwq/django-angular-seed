f9b37c9898b65ad179570124f52d8b11a0c2c1b8
Jeffrey Quinn <jeffreyquinn@Jeffreys-MacBook-Air.local>
2014-02-09 21:12:56 -0800

    Revert "adding d3"
    This reverts commit 7e1f62bdc8bc4958875a8aadcb628872945eec94.

--- app/static/controllers/main.js
+++ app/static/controllers/main.js
@@ -5,13 +5,10 @@ App.config(['$routeProvider', function($routeProvider) {
       .when('/', {
         templateUrl: '/static/views/main.html',
         controller: 'MainCtrl'
-      })
-      .when('/test', {
-        templateUrl: '/static/views/test.html',
       });
 }]);
 
-App.controller('MainCtrl', function($scope, $location) {
+App.controller('MainCtrl', function($scope) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
@@ -20,25 +17,4 @@ App.controller('MainCtrl', function($scope, $location) {
     'django-south',
     'django-compressor'
   ];
-    $scope.dest = '';
-    $scope.some_number = 0;
-    $scope.change_view = function(dest) {
-	$location.url('/'+dest);
-    };
-    $scope.show_graphic = function() {
-
-	var sampleSVG = d3.select("#viz")
-	    .append("svg")
-	    .attr("width", 100)
-	    .attr("height", 100);    
-
-	sampleSVG.append("circle")
-	    .style("stroke", "gray")
-	    .style("fill", "white")
-	    .attr("r", 40)
-	    .attr("cx", 50)
-	    .attr("cy", 50)
-	    .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-	    .on("mouseout", function(){d3.select(this).style("fill", "white");});
-    };
 });
