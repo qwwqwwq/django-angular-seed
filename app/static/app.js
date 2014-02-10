7e1f62bdc8bc4958875a8aadcb628872945eec94
Jeffrey Quinn <jeffreyquinn@Jeffreys-MacBook-Air.local>
2014-02-09 20:07:33 -0800

    adding d3

--- app/static/app.js
+++ app/static/app.js
@@ -36,3 +36,33 @@ App.directive('waffleSwitch', function() {
         }
     };
 });
+
+App.directive('myNodes', ['$compile', function ($compile) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var nodes = [{"name": "foo"}, {"name": "bar"}] 
+            var mySvg = d3.select(element[0])
+                  .append("svg")
+                  .attr("width", 100)
+                  .attr("height", 100);
+
+            var node = mySvg.selectAll(".node")
+             .data(nodes)
+             .enter()
+             .append("circle")
+             .attr("cx", function(d,i){
+                return 20+i*50;
+             })
+             .attr("cy", 50)
+             .attr("r", 10)
+             .attr("tooltip-append-to-body", true)
+             .attr("tooltip", function(d){
+                 return d.name;
+             });
+
+            element.removeAttr("my-nodes");
+            $compile(element)(scope);
+            }
+        };
+    }]);
--- app/static/controllers/main.js
+++ app/static/controllers/main.js
@@ -5,10 +5,13 @@ App.config(['$routeProvider', function($routeProvider) {
       .when('/', {
         templateUrl: '/static/views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/test', {
+        templateUrl: '/static/views/test.html',
       });
 }]);
 
-App.controller('MainCtrl', function($scope) {
+App.controller('MainCtrl', function($scope, $location) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
@@ -17,4 +20,25 @@ App.controller('MainCtrl', function($scope) {
     'django-south',
     'django-compressor'
   ];
+    $scope.dest = '';
+    $scope.some_number = 0;
+    $scope.change_view = function(dest) {
+	$location.url('/'+dest);
+    };
+    $scope.show_graphic = function() {
+
+	var sampleSVG = d3.select("#viz")
+	    .append("svg")
+	    .attr("width", 100)
+	    .attr("height", 100);    
+
+	sampleSVG.append("circle")
+	    .style("stroke", "gray")
+	    .style("fill", "white")
+	    .attr("r", 40)
+	    .attr("cx", 50)
+	    .attr("cy", 50)
+	    .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+	    .on("mouseout", function(){d3.select(this).style("fill", "white");});
+    };
 });
